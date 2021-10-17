1e53f34d1d561bf2b8140a40b15c084b3d774783
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2021-10-17 21:06:00 +0330

    add summation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     while command != "exit":
         ops = command.split(' ')
         # recognize the command
-
+        if ops[0] == "+":
+            print(int(ops[1]) + int(ops[2]))
         command = input().strip()
     print("Thanks for choosing us :)")
