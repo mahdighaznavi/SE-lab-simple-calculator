8783735f05e197b0ec92c01fed9aae2dc98f2469
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2021-10-17 21:02:19 +0330

    add starter file

---
+++ main.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    print("Welcome to our simple calculator")
+    command = input().strip()
+    while command != "exit":
+        ops = command.split(' ')
+        # recognize the command
+
+        command = input().strip()
+    print("Thanks for choosing us :)")
